fb5bb8d24f44bd177020fd5e3442c116d99a0b4c
John B. McEwan <jbmcewan@tower.tarot>
2016-03-02 19:43:58 -0800

    Merged changes between trees.

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 /* global describe, it */
 import {expect} from 'chai';
-import starWars from './index';
+import starWars from '../src/index';
 
 describe('jbm-starwars-names', function(){
     describe('all', function() {
